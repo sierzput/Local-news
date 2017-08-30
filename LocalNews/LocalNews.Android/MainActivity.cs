906c392502b6bb2b09dce29ce6aabe68186b89dd
Paweł Sierzputowski <sierzput@gmail.com>
2017-08-30 04:42:02 +0200

    Merge solutions

--- LocalNews/LocalNews.Android/MainActivity.cs
+++ LocalNews/LocalNews.Android/MainActivity.cs
@@ -1,24 +1,22 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace LocalNews.Droid
 {
-    [Activity(Label = "LocalNews", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    [Activity(Label = "@string/app_name", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+
             LoadApplication(new App());
         }
     }
 }
-
---
+++ LocalNews/LocalNews.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LocalNews.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new LocalNews.App());
+        }
+    }
+}
