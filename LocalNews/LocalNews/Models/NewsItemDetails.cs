b96bb6d0f55705341cb1eda891324c1184d15de9
Paweł Sierzputowski <sierzput@gmail.com>
2017-09-05 01:40:01 +0200

    Add data classes

---
+++ LocalNews/LocalNews/Models/NewsItemDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace LocalNews.Models
+{
+    public class NewsItemDetails
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public List<string> Thumbnails { get; set; }
+        public string PublicationDate { get; set; }
+        public string FullArticle { get; set; }
+        public string Author { get; set; }
+        public string SourceLink { get; set; }
+    }
+}
---
+++ LocalNews/LocalNews/Models/NewsListItem.cs
@@ -0,0 +1,12 @@
+namespace LocalNews.Models
+{
+    public class NewsListItem
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Thumbnail { get; set; }
+        public string PublicationDate { get; set; }
+        public string Summary { get; set; }
+        public string DetailsLink { get; set; }
+    }
+}
