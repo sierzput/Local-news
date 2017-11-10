4a9514ffd7d8590b24949f622cbd46c4898b0608
Paweł Sierzputowski <sierzput@gmail.com>
2017-11-10 19:01:09 +0100

    Add images carousel view to detail page

--- LocalNews/LocalNews/ViewModels/ItemDetailViewModel.cs
+++ LocalNews/LocalNews/ViewModels/ItemDetailViewModel.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Castle.Core.Internal;
 using LocalNews.Models;
 using LocalNews.Services;
 
@@ -6,10 +8,19 @@ namespace LocalNews.ViewModels
     public class ItemDetailViewModel : BaseViewModel
     {
         public NewsListItem Item { get; set; }
+        public IEnumerable<string> Images { get; }
+
         public ItemDetailViewModel(NewsListItem item, IDataStore<NewsListItem> dataStore) : base(dataStore)
         {
             Title = item.Title;
             Item = item;
+            var images = new List<string>();
+            if (!item.Thumbnail.IsNullOrEmpty())
+            {
+                images.Add(item.Thumbnail);
+                images.Add(item.Thumbnail);
+            }
+            Images = images;
         }
     }
 }
